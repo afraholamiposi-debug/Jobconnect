8e37e3f531f182c55f0f3cb2938e72f1cd7816bf
afraholamiposi-debug <afraholamiposi@gmail.com>
2026-08-15 23:18:19 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -71,6 +71,8 @@ ${message}
 
 const PORT = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
